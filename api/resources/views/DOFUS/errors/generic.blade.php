865238adb1120de50dad1acae76879e6ffbc8ab9
LuaxY <yann.guineau@voidmx.net>
2016-08-12 14:38:09 +0200

    Improve generic errors view

--- api/resources/views/DOFUS/errors/generic.blade.php
+++ api/resources/views/DOFUS/errors/generic.blade.php
@@ -2,6 +2,10 @@
 @include('layouts.menus.base')
 
 @section('content')
-    <h1>{{ $title }}</h1>
-    <p>{{ $detail }}</p>
+    <div class="content">
+        <h1 class="content-title">
+            <span class="icon-big icon-devblog"></span> {{ $title }}
+        </h1>
+        <p style="padding: 10px">{{ $detail }}</p>
+    </div> <!-- content -->
 @stop
