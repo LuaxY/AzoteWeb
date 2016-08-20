e35a7e19e070b6725c8f8b9097b9cefabf6ba688
LuaxY <yann.guineau@voidmx.net>
2016-08-20 13:56:22 +0200

    Fix notify flash error in vote

--- api/app/Http/Controllers/VoteController.php
+++ api/app/Http/Controllers/VoteController.php
@@ -63,7 +63,7 @@ class VoteController extends Controller
 
         if (Auth::user()->votes % 10 == 0)
         {
-            $request->session()->flash('notify', ['type' => 'success', 'message' => "Vous avez reçus un nouveau cadeau !"]);
+            request()->session()->flash('notify', ['type' => 'success', 'message' => "Vous avez reçus un nouveau cadeau !"]);
             $reward = VoteReward::where('votes', Auth::user()->votes)->firstOrFail();
             // TODO: add $reward->itemId to account
         }
