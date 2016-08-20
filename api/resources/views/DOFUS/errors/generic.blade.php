a60436f91f7da67e6d7f7c6c103125efe63ff01f
LuaxY <yann.guineau@voidmx.net>
2016-08-20 10:41:15 +0200

    Fix template for generic errors page

--- api/resources/views/DOFUS/errors/generic.blade.php
+++ api/resources/views/DOFUS/errors/generic.blade.php
@@ -1,11 +1,24 @@
 @extends('layouts.contents.default')
 @include('layouts.menus.base')
 
+@section('breadcrumbs')
+{!! Breadcrumbs::render('page', 'Erreur') !!}
+@stop
+
 @section('content')
-    <div class="content">
-        <h1 class="content-title">
-            <span class="icon-big icon-devblog"></span> {{ $title }}
-        </h1>
-        <p style="padding: 10px">{{ $detail }}</p>
-    </div> <!-- content -->
+<div class="ak-container ak-main-center">
+    <div class="ak-title-container">
+        <h1><span class="ak-icon-big ak-devblog"></span></a> {{ $title }}</h1>
+    </div>
+
+    <div class="ak-container ak-panel-stack">
+        <div class="ak-container ak-panel ak-glue">
+            <div class="ak-panel-content">
+                <div class="panel-main">
+                    {{ $detail }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @stop
