7ac905bbf71b19443481c584d96792648cf6c294
LuaxY <yann.guineau@voidmx.net>
2016-09-13 20:02:35 +0200

    Report PHP error to web developer email in production env

--- api/app/Exceptions/Handler.php
+++ api/app/Exceptions/Handler.php
@@ -12,6 +12,10 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 use App\Exceptions\GenericException;
 
+use Config;
+use Mail;
+use Carbon\Carbon;
+
 class Handler extends ExceptionHandler
 {
     /**
@@ -37,6 +41,31 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $e)
     {
+        if ($e instanceof Exception && Config::get('app.env') == 'production')
+        {
+            $debug = Config::get('app.debug');
+            Config::set('app.debug', true);
+
+            if (ExceptionHandler::isHttpException($e))
+            {
+                $content = ExceptionHandler::toIlluminateResponse(ExceptionHandler::renderHttpException($e), $e);
+            }
+            else
+            {
+                $content = ExceptionHandler::toIlluminateResponse(ExceptionHandler::convertExceptionToResponse($e), $e);
+            }
+
+            Config::set('app.debug', $debug);
+
+            $data['content'] = (!isset($content->original)) ? $e->getMessage() : $content->original;
+
+            Mail::send('emails.report', $data, function ($message) {
+                $message->from(config('mail.sender'), 'Azote.us');
+                $message->to('kerubim@azote.us', 'Web Developer');
+                $message->subject('Azote.us - PHP Error Report - ' . Carbon::now());
+            });
+        }
+
         return parent::report($e);
     }
 
