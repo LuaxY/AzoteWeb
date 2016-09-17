64cd2cc97eb5d8216f911a734416b9379165bec6
LuaxY <yann.guineau@voidmx.net>
2016-09-17 16:25:26 +0200

    New email template

--- api/resources/views/DOFUS/emails/password.blade.php
+++ api/resources/views/DOFUS/emails/password.blade.php
@@ -1,13 +1,13 @@
-<style>
-* {
-    font-family: "Helvetica", "Arial", sans-serif;
-}
-</style>
-<h3>Bonjour {{ $user->firstname }} {{ $user->lastname }} !</h3>
-<p>Vous venez de demander une réinitialisation de votre mot de passe Azote.us.<p>
-<p>Si vous êtes bien l'auteur de cette demande, il reste une dernière étape pour valider le changement. Cliquez sur le lien ci-dessous:</p>
-<br>
-<a href="{{ route('reset', $user->ticket) }}">Cliquez-ici pour changer de mot de passe</a>
+@extends('layouts.emails.template')
+
+@section('content')
+<b>Bonjour {{ $user->firstname }} {{ $user->lastname }} !</b>
 <br><br>
-<p>Si vous n'avez pas fait cette demande, ignorez cet email et n'hésitez à prévenir le support.</p>
-<p><b>Azote.us</b></p>
+Vous venez de demander une réinitialisation de votre mot de passe {{ config('dofus.title') }}.
+<br><br>
+Si vous êtes bien l'auteur de cette demande, il reste une dernière étape pour valider le changement. Cliquez sur le lien ci-dessous:
+<br><br>
+<a style="color:#00bcc5;font-weight:bold;text-decoration:none;" href="{{ route('reset', $user->ticket) }}">Cliquez-ici pour changer de mot de passe</a>
+<br><br>
+Si vous n'avez pas fait cette demande, ignorez cet email et n'hésitez à prévenir le support.
+@stop
