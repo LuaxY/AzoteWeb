1f5f40ff184a84627535ad7024aa0d819afea1b6
LuaxY <yann.guineau@voidmx.net>
2016-09-25 20:03:52 +0200

    WIP

--- api/app/Providers/AuthServiceProvider.php
+++ api/app/Providers/AuthServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use App\Permission;
 
-use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -23,16 +23,15 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * Register any application authentication / authorization services.
      *
-     * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
      * @return void
      */
-    public function boot(GateContract $gate)
+    public function boot()
     {
-        $this->registerPolicies($gate);
+        $this->registerPolicies();
 
         foreach ($this->getPermissions() as $permission)
         {
-            $gate->define($permission->name, function($user) use($permission) {
+            Gate::define($permission->name, function($user) use($permission) {
                 return $user->hasRole($permission->roles);
             });
         }
