de18c7b2250395433da53c30e28c42ececc69f1e
LuaxY <yann.guineau@voidmx.net>
2016-09-27 23:18:23 +0200

    Ladder without GM

--- api/app/Http/Controllers/PageController.php
+++ api/app/Http/Controllers/PageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use \Cache;
+use \DB;
 
 use App\Character;
 
@@ -20,7 +21,21 @@ class PageController extends Controller
     public function ladder()
     {
         $characters = Cache::remember('ladder', self::LADDER_CACHE_EXPIRE_MINUTES, function() {
-            return Character::on('sigma_world')->orderBy('Experience', 'DESC')->take(100)->get();
+            $server = 'sigma';
+            $db = config('database.connections');
+            $auth  = $db[$server.'_auth']['database'];
+            $world = $db[$server.'_world']['database'];
+
+            $result = DB::table($world.'.characters AS ch')
+                ->select('ch.*')
+                ->leftJoin($auth.'.worlds_characters AS wc', 'ch.Id', '=', 'wc.CharacterId')
+                ->leftJoin($auth.'.accounts AS acc', 'wc.AccountId', '=', 'acc.Id')
+                ->where('acc.UserGroupId', 1)
+                ->orderBy('ch.Experience', 'DESC')
+                ->take(100)
+                ->get();
+
+            return Character::hydrate($result);
         });
 
         return view('pages.ladder', ['characters' => $characters]);
