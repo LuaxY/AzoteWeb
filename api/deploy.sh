3d7dcf03a9e0fa5706f3a932f7247a0c039e8652
LuaxY <yann.guineau@voidmx.net>
2016-10-08 17:51:49 +0200

    Put website in maintenance mode during deployment

--- api/deploy.sh
+++ api/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 git pull
+php artisan down
 php artisan migrate
 gulp --production
 chmod -R 777 storage bootstrap/cache public/forge public/uploads public/filemanager/userfiles
+php artisan optimize
+chmod -R 777 storage bootstrap/cache public/forge public/uploads public/filemanager/userfiles
+php artisan up
