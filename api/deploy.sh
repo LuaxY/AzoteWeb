f1aa24cb60cde9128c9f479fb948cf756fd4cecc
Kerubim <kerubim@azote.us>
2016-10-26 15:06:16 +0200

    Update deploy.sh

--- api/deploy.sh
+++ api/deploy.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 php artisan down
+
 git pull
 php artisan migrate
 gulp --production
-chmod -R 777 storage bootstrap/cache public/forge public/uploads public/filemanager/userfiles
+
+chmod -R 777 bootstrap/cache public/forge public/uploads public/filemanager/userfiles
+
+chmod -R 777 storage
+mkdir -p storage/framework/sessions
+chmod -R 777 storage
+
+php artisan clear-compiled 
+php artisan dump-autoload
 php artisan optimize
-chmod -R 777 storage bootstrap/cache public/forge public/uploads public/filemanager/userfiles
+
 php artisan up
