0188bde1137d5742b6be0363245e7b62cd43f5de
Kerubim <kerubim@azote.us>
2016-10-31 18:11:47 +0100

    Fix support route

--- api/resources/views/DOFUS/emails/email-changed.blade.php
+++ api/resources/views/DOFUS/emails/email-changed.blade.php
@@ -7,5 +7,5 @@ Vous venez de demander un changement d'adresse email {{ config('dofus.title') }}
 <br><br>
 Si vous êtes bien l'auteur de cette demande, vous pouvez ignorer cet email, dans le cas contraire merci de prendre contact avec le support. Cliquez sur l'un lien ci-dessous:
 <br><br>
-<a style="color:#00bcc5;font-weight:bold;text-decoration:none;" href="{{ route('support') }}">Cliquez-ici pour contacter le support</a>
+<a style="color:#00bcc5;font-weight:bold;text-decoration:none;" href="{{ URL::to('support') }}">Cliquez-ici pour contacter le support</a>
 @stop
