2b490ca0474024e9d9642a582a5f8e8cf1ea87f1
AZOTE\destiny <destiny@azote.us>
2017-05-26 16:43:27 +0200

    - Fix bug with avatars

--- api/app/Http/Controllers/AccountController.php
+++ api/app/Http/Controllers/AccountController.php
@@ -494,10 +494,11 @@ class AccountController extends Controller
 
                 $link = DofusForge::player($character,$requested_avatar['server'], 'face', 1, 100, 100);
                 $path = 'uploads/users/'.Auth::user()->id;
+                $timestamp = Carbon::now()->timestamp;
                 if(!File::exists($path))
                     File::makeDirectory($path);
-                File::copy($link, 'uploads/users/'.Auth::user()->id.'/'.$requested_avatar['characterid'].'-'.$requested_avatar['server'].'.png');
-                Auth::user()->avatar = 'uploads/users/'.Auth::user()->id.'/'.$requested_avatar['characterid'].'-'.$requested_avatar['server'].'.png';
+                File::copy($link, 'uploads/users/'.Auth::user()->id.'/'.$timestamp.'-'.$requested_avatar['characterid'].'-'.$requested_avatar['server'].'.png');
+                Auth::user()->avatar = 'uploads/users/'.Auth::user()->id.'/'.$timestamp.'-'.$requested_avatar['characterid'].'-'.$requested_avatar['server'].'.png';
             }
             else
             {
